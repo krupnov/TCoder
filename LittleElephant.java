88f776206f7db0b07a060432f56e6895d167fbf5
krupnov <krupnov.iv@gmail.com>
2015-05-15 13:30:56 +0300

    Update LittleElephant.java

--- LittleElephant.java
+++ LittleElephant.java
@@ -4,10 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-
+ 
 public class LittleElephant {
-	
-	private static Integer[][][][] memo;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -27,10 +25,10 @@ public class LittleElephant {
 			}
 			int result = 0;
 			for (int p = 0 ; p < 10 ; ++p) {
-				memo = new Integer[N][M][10][4];
 				int localMaximum = 0;
+				int[][][][] pp = computeMaxPathes(A, N, M, p);
 				for (int i = 0 ; i < 4 ; ++i) {
-					int temp = compute(N, M, A, p, 0, 0, 9, i);
+					int temp = pp[0][0][9][i];
 					if (localMaximum < temp) {
 						localMaximum = temp;
 					}
@@ -42,94 +40,7 @@ public class LittleElephant {
 			System.out.println(result);
 		}
 	}
-
-	private static int compute(int N, int M, int[][] A, int p, int n, int m, int bound, int add) {
-		if (memo[n][m][bound][add] != null) {
-			return memo[n][m][bound][add];
-		}
-
-		int current = A[n][m];
-		if (add > 0) {
-			current = (current + p) % 10;
-		}
-		if (add > 2) {
-			current = (current + p) % 10;
-		}
-		
-		ArrayList<int[]> nextCoords = new ArrayList<int[]>();
-		ArrayList<int[]> additions = new ArrayList<int[]>();
-		if (n < N - 1) {
-			int[] e = new int[2];
-			e[0] = n + 1;
-			e[1] = m;
-			nextCoords.add(e);
-			int[] e2 = new int[2];
-			if (add == 0 || add == 2) {
-				e2[0] = 0;
-				e2[1] = 2;
-			}
-			if (add == 1 || add == 3) {
-				e2[0] = 1;
-				e2[1] = 3;
-			}
-			additions.add(e2);
-		}
-		
-		if (m < M - 1) {
-			int[] e = new int[2];
-			e[0] = n;
-			e[1] = m + 1;
-			nextCoords.add(e);
-			int[] e2 = new int[2];
-			if (add == 0 || add == 1) {
-				e2[0] = 0;
-				e2[1] = 1;
-			}
-			if (add == 2 || add == 3) {
-				e2[0] = 2;
-				e2[1] = 3;
-			}
-			additions.add(e2);
-		}
-		ArrayList<int[]> nextBounds = new ArrayList<int[]>();
-		if (current == bound) {
-			int[] e = new int[2];
-			e[0] = current;
-			e[1] = 1;
-			nextBounds.add(e);
-		}
-		if (current > bound) {
-			int[] e = new int[2];
-			e[0] = bound;
-			e[1] = 0;
-			nextBounds.add(e);
-		}
-		if (current < bound) {
-			int[] e = new int[2];
-			e[0] = bound;
-			e[1] = 0;
-			nextBounds.add(e);
-			e = new int[2];
-			e[0] = current;
-			e[1] = 1;
-			nextBounds.add(e);
-		}
-		int result = current <= bound ? 1 : 0;
-		for (int t = 0 ; t < nextBounds.size() ; ++t) {
-			for (int k = 0 ; k < nextCoords.size() ; ++k) {
-				int[] addition = additions.get(k);
-				int[] nextCoord = nextCoords.get(k);
-				for (int j = 0 ; j < addition.length ; ++j) {
-					result = Math.max(result,
-									nextBounds.get(t)[1] + compute(N, M, A, p,
-									nextCoord[0],nextCoord[1],nextBounds.get(t)[0],addition[j]));
-				}
-			}
-		}
-		memo[n][m][bound][add] = result;
-		return result;
-	}
-
+ 
 	private static int[][][][] computeMaxPathes(int[][] A, int N, int M, int p) {
 		int[][][][] maxPathes = new int[N][M][10][4];
 		for (int n = N - 1; n >= 0 ; --n) {
@@ -145,20 +56,20 @@ public class LittleElephant {
 						}
 						
 						ArrayList<int[]> nextCoords = new ArrayList<int[]>();
-						ArrayList<ArrayList<Integer>> additions = new ArrayList<ArrayList<Integer>>();
+						ArrayList<int[]> additions = new ArrayList<int[]>();
 						if (n < N - 1) {
 							int[] e = new int[2];
 							e[0] = n + 1;
 							e[1] = m;
 							nextCoords.add(e);
-							ArrayList<Integer> e2 = new ArrayList<Integer>();
+							int[] e2 = new int[2];
 							if (j == 0 || j == 2) {
-								e2.add(0);
-								e2.add(2);
+								e2[0] = 0;
+								e2[1] = 2;
 							}
 							if (j == 1 || j == 3) {
-								e2.add(1);
-								e2.add(3);
+								e2[0] = 1;
+								e2[1] = 3;
 							}
 							additions.add(e2);
 						}
@@ -168,51 +79,29 @@ public class LittleElephant {
 							e[0] = n;
 							e[1] = m + 1;
 							nextCoords.add(e);
-							ArrayList<Integer> e2 = new ArrayList<Integer>();
+							int[] e2 = new int[2];
 							if (j == 0 || j == 1) {
-								e2.add(0);
-								e2.add(1);
+								e2[0] = 0;
+								e2[1] = 1;
 							}
 							if (j == 2 || j == 3) {
-								e2.add(2);
-								e2.add(3);
+								e2[0] = 2;
+								e2[1] = 3;
 							}
 							additions.add(e2);
 						}
-						ArrayList<int[]> nextCaps = new ArrayList<int[]>();
-						if (current == i) {
-							int[] e = new int[2];
-							e[0] = current;
-							e[1] = 1;
-							nextCaps.add(e);
-						}
-						if (current > i) {
-							int[] e = new int[2];
-							e[0] = i;
-							e[1] = 0;
-							nextCaps.add(e);
+						int result = current == i ? 1 : 0;
+						for (int k = 0 ; k < nextCoords.size() ; ++k) {
+							int[] addition = additions.get(k);
+							int[] nextCoord = nextCoords.get(k);
+							for (int jj = 0 ; jj < addition.length ; ++jj) {
+								result = Math.max(result,
+												(current == i ? 1 : 0) + maxPathes
+												[nextCoord[0]][nextCoord[1]][i][addition[jj]]);
+							}
 						}
 						if (current < i) {
-							int[] e = new int[2];
-							e[0] = i;
-							e[1] = 0;
-							nextCaps.add(e);
-							e = new int[2];
-							e[0] = current;
-							e[1] = 1;
-							nextCaps.add(e);
-						}
-						int result = current <= i ? 1 : 0;
-						for (int t = 0 ; t < nextCaps.size() ; ++t) {
-							for (int k = 0 ; k < nextCoords.size() ; ++k) {
-								ArrayList<Integer> addition = additions.get(k);
-								for (int jj = 0 ; jj < addition.size() ; ++jj) {
-									int[] nextCoord = nextCoords.get(k);
-									result = Math.max(result,
-													nextCaps.get(t)[1] + maxPathes
-													[nextCoord[0]][nextCoord[1]][nextCaps.get(t)[0]][addition.get(jj)]);
-								}
-							}
+							result = Math.max(result, maxPathes[n][m][current][j]);
 						}
 						maxPathes[n][m][i][j] = result;
 					}
