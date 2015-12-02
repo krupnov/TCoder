315e952fa19ca2dfb99b0807f569f557a7a5212e
krupnov <krupnov.iv@gmail.com>
2015-12-02 11:52:01 +0300

    Create eufunction.cpp

---
+++ eufunction.cpp
@@ -0,0 +1,19 @@
+/*
+ * eufunction.cpp
+ *
+ *  Created on: Dec 2, 2015
+ *      Author: ilya
+ */
+#include <iostream>
+
+typedef unsigned long long number;
+
+int main() {
+	int T;
+	std::cin>>T;
+	for (int t = 0 ; t < T ; ++t) {
+		number A,D,L,R;
+		std::cin>>A>>D>>L>>R;
+		
+	}
+}
